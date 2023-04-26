c5402289f0f76205b9a939e73613b0bf4011f7d1
murali ithanaboina <murali.ithanaboina1998@gmail.com>
2023-04-26 20:28:10 +0530

    modified jio

--- src/main/java/eventmanagement/eventmanagement/EventmanagementApplication.java
+++ src/main/java/eventmanagement/eventmanagement/EventmanagementApplication.java
@@ -2,9 +2,14 @@ package eventmanagement.eventmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages = "eventmanagement.eventmanagement",
+		excludeFilters={@ComponentScan.Filter(type = FilterType.ASPECTJ,pattern = "eventmanagement.eventmanagement.exception.*")} )
 
 public class EventmanagementApplication {
 
